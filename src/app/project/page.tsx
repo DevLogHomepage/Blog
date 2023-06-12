64a9754f76dba70a91e3a724b8209b2909d25401
dennis ko <dennis0324@naver.com>
2023-06-13 04:48:17 +0900

    remove: duplicated style.css

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,6 +1,5 @@
 import notion from '../../../lib/notion'
 import { NotionPage } from "@/components/NotionPage"
-import "@/app/globals_notion.css"
 
 export default async function page(){
   const recordMap = await notion.getPage("25a989bf-7c76-4fbf-876c-8885f5f4ce34")

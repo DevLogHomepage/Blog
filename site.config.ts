930e1ae5d3a18e9d545fa673b0c9ebefa0751c29
dennis ko <dennis0324@naver.com>
2023-06-18 01:13:03 +0900

    change: decompose github structure

--- site.config.ts
+++ site.config.ts
@@ -1,18 +1,6 @@
-// 1. 블로그 사이트 이름
-// 2. 블로그 도메인
-// 3. router -> {
-// 	id: 
-// 	pageRoute
-// }
-// 4. 
-// 5. .env
-//  5-1. github token
-//  5-2. github 아이디
-//  5-3. github repo
-import { siteConfig } from './lib/siteConfig'
-import "dotenv/config"
+import { siteConfig } from './lib/site-config'
 
-siteConfig({
+export default siteConfig({
   siteName: 'foxstar Devlog',
 
   // siteDomain: 'https://foxstar-devlog.netlify.app',
@@ -22,11 +10,6 @@ siteConfig({
     {id:'PROJECT',pageRoute:"/project"},
     {id:'ABOUT',pageRoute:'/about'}
   ],
-  github:{
-    token: process.env.GITHUB_TOKEN ?? '',
-    username: process.env.GITHUB_USERNAME ?? '',
-    repo: process.env.GITHUB_REPO ?? ''
-  }
+  githubRepo: 'blogPost',
+  githubUsername: 'dennis0324'
 })
-
-export { siteConfig }

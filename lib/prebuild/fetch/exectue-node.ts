b21621b9cebf6894341188bda594d5f9b0b4641e
dennis ko <dennis0324@naver.com>
2023-07-03 17:27:33 +0900

    add: clear unecessary code

--- lib/prebuild/fetch/exectue-node.ts
+++ lib/prebuild/fetch/exectue-node.ts
@@ -1,26 +1,31 @@
 import path from "path"
 import { NodeAction } from "lib/actions"
 import fs from 'fs'
-import { nextqlWorker,Worker } from "lib/nextql-worker"
+import { nextqlWorker,Worker } from "@lib/worker/lib/nextql-worker"
 
 const getPirority = () => 1
 
 export { getPirority }
 
-// const userNode = {
-//   id: `${i}`,
-//   parent: `__SOURCE__`,
-//   internal: {
-//     type: `Post`, // name of the graphQL query --> allPost {}
-//     contentDigest:''
-//   },
-//   children: [],
-//   title:metaJson.title,
-//   content:content,
-//   tags:metaJson.tags,
-//   dates:dates
-// }
 
+/**
+ * 
+ * @param actions 
+ * @example
+ * const userNode = {
+    id: `${i}`,
+    parent: `__SOURCE__`,
+    internal: {
+      type: `Post`, // name of the graphQL query --> allPost {}
+      contentDigest:''
+    },
+    children: [],
+    title:metaJson.title,
+    content:content,
+    tags:metaJson.tags,
+    dates:dates
+  }
+ */
 
 export default async function execute(actions:NodeAction){
   const nextFile = fs.readdirSync(`${path.resolve(".")}/.nextql`)
@@ -31,11 +36,19 @@ export default async function execute(actions:NodeAction){
     catch(e){
     }
   }
-  const dbWorker = nextqlWorker(path.join(path.resolve("."),'/lib/data-manager/lmdb-worker.ts'), {})
-  const worker = nextqlWorker(path.join(path.resolve("."),'/lib/nextqlNode-worker.ts'), {})
+  const dbWorker = nextqlWorker(path.join(path.resolve("."),'/lib/worker/worker-module/lmdb-worker.ts'), {})
+  const worker = nextqlWorker(path.join(path.resolve("."),'/lib/worker/worker-module/nextqlNode-worker.ts'), {})
   worker.on("message",([type,value]) => {
     dbWorker.postMessage([type,value])
     // console.log(value)
+  })
+
+  worker.on('exit',() => {
+    console.log('nextqlNode-worker exit')
+  })
+
+  dbWorker.on('exit',()=> {
+    console.log('lmdb-worker exit')
   })
 }
 
